a3db49a28e9895e88a29e6c84615b8c18f92757c
Attila Szabó <lindi.2brothers@gmail.com>
2023-10-08 09:35:30 +0200

    Stress relief

---
+++ script.js
@@ -0,0 +1,15 @@
+var button_to_count = document.querySelector(".button");
+var reset_to_count = document.querySelector(".reset");
+var number_display = document.querySelector(".szamlalo");
+var number = 0;
+
+button_to_count.addEventListener("click", () => {
+    couting = number += 1;
+    number_display.innerText = couting;
+});
+
+reset_to_count.addEventListener("click", () => {
+    number = 0;
+    number_display.innerText = number;
+});
+
